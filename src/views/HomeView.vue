71d2e7b0e64dae22402b1ce62be3c3fb0da3ce3d
Danila Nefedov <dn375@mail.ru>
2022-10-25 17:19:43 +0400

    created component BlogRoll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home"></div>
+  <BlogRoll />
 </template>
 <script>
+import BlogRoll from "@/components/site/blog/BlogRoll.vue";
 export default {
   name: "HomeView",
+  components: {
+    BlogRoll,
+  },
 };
 </script>
