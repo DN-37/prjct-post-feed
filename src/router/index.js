85b1caff1ebf30c148337555f3ca2aa4b1cefcf3
Danila Nefedov <dn375@mail.ru>
2022-10-25 17:21:31 +0400

    created component Post

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
+import Post from "../components/site/blog/Post.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/post/:id",
+    name: "Post",
+    component: Post,
   },
 ];
 
